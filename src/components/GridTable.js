ccb01fd1461c549fc62bb5ac443fdb5b438a852d
unknown <tranhongquan2106@gmail.com>
2021-06-17 22:14:35 +0700

    try to replicate dijkstra algo like clement and understand it

--- src/components/GridTable.js
+++ src/components/GridTable.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 
 import GridNode from './GridNode/GridNode'
+
+import {dijkstra, getNodesInShortestPathOrder} from '../algos/dijkstra';
 
 const COLUMNS = 59;
 const ROWS = 15;
@@ -41,9 +43,47 @@ const createNode = (row, col) => {
     };
 } 
 
+function animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder) {
+    for (let i = 0; i <= visitedNodesInOrder.length; i++) {
+      if (i === visitedNodesInOrder.length) {
+        setTimeout(() => {
+          animateShortestPath(nodesInShortestPathOrder);
+        }, 10 * i);
+        return;
+      }
+      setTimeout(() => {
+        const node = visitedNodesInOrder[i];
+        document.getElementById(`node-${node.row}-${node.col}`).className =
+          'node node-visited';
+      }, 10 * i);
+    }
+  }
+
+function animateShortestPath(nodesInShortestPathOrder) {
+    for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
+      setTimeout(() => {
+        const node = nodesInShortestPathOrder[i];
+        document.getElementById(`node-${node.row}-${node.col}`).className =
+          'node node-shortest-path';
+      }, 50 * i);
+    }
+  }
+
+
+function visualizeDijkstra(gridTable) {
+    console.log('run')
+    const startNode = gridTable[START_NODE_ROW][START_NODE_COL];
+    const finishNode = gridTable[FINISH_NODE_ROW][FINISH_NODE_COL];
+    const visitedNodesInOrder = dijkstra(gridTable, startNode, finishNode);
+    const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
+    animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
+}
+
+
+
 
 function GridTable() {
-    const [gridTable, setGridTable] = useState(gridTableInit(ROWS, COLUMNS));
+    const gridTable = gridTableInit(ROWS, COLUMNS);
 
 //    useEffect(() => {
        
@@ -74,6 +114,7 @@ function GridTable() {
                     )
                 })
             }
+            <button onClick={() => visualizeDijkstra(gridTable)}>Dijkstra</button>
         </div>
     )
 }
